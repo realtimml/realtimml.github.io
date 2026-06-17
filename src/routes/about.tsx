a0048ebe47418fd2a949958b2230957bff170391
realtimml <timmyphan066@gmail.com>
2026-06-17 00:19:18 -0700

    Add neofetch animations

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -9,8 +9,10 @@ function AboutPage() {
   return (
     <section className="min-h-screen w-full bg-[url(src/assets/imgs/Backgrounds/ground.jpg)] bg-cover bg-position-[100%_30%] brightness-80">
       <div className="min-h-screen backdrop-blur-md px-28 py-20 font-monospace text-2xl">
-        <p>remote-user@timml.net ~ % neofetch</p>
-        <div className='flex items-center gap-2'>
+        <p className="w-fit overflow-hidden whitespace-nowrap animate-typing">
+          remote-user@timml.net ~ % neofetch
+        </p>
+        <div className="flex items-center gap-2 animate-reveal">
           <img src={doodle} alt="Timmy Phan" className="w-100 h-100" />
           <div>
             <p>Timmy Phan</p>
@@ -24,7 +26,9 @@ function AboutPage() {
             <p>technologies: Python, HTML/JS/CSS, React, TypeScript, Figma</p>
           </div>
         </div>
-        <p>remote-user@timml.net ~ %</p>
+        <p className="w-fit border-r-7 border-r-transparent pr-[1ch] animate-caret-reveal">
+          remote-user@timml.net ~ %
+        </p>
       </div>
     </section>
   )
