9a5b1183c92a67d49c28afe80de1e7b7ea3f8b48
realtimml <timmyphan066@gmail.com>
2026-06-16 20:52:31 -0700

    Add hover animations to contact links

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -10,23 +10,30 @@ function ContactPage() {
 
   return (
     <section className="min-h-screen w-full bg-[#9EA1D4] px-28 pt-20">
-      <Link className="flex justify-between" to="https://github.com/realtimml">
+      <Link className="flex justify-between group" to="https://github.com/realtimml">
         <RiGithubFill className="w-40 h-40" />
-        <span className="text-9xl font-bold">GitHub</span>
+        <span className="text-9xl font-bold">
+          GitHub
+          <div className="bg-white h-2 w-0 group-hover:w-full transition-all duration-300 ml-auto"></div>
+          </span>
       </Link>
-      <Link className="flex justify-between" to="https://www.linkedin.com/in/timmy-p/">
+      <Link className="flex justify-between group" to="https://www.linkedin.com/in/timmy-p/">
         <RiLinkedinBoxFill className="w-40 h-40" />
-        <span className="text-9xl font-bold">LinkedIn</span>
+        <span className="text-9xl font-bold group">LinkedIn
+          <div className="bg-white h-2 w-0 group-hover:w-full transition-all duration-300 ml-auto"></div>
+          </span>
       </Link>
       <Link
-        className="flex justify-between"
+        className="flex justify-between group"
         onClick={(e) => {
           e.preventDefault();
           window.location.href = `mailto:${atob(encoded)}`;
         }}
       >
         <RiMailFill className="w-40 h-40" />
-        <span className="text-9xl font-bold">Email</span>
+        <span className="text-9xl font-bold group">Email
+          <div className="bg-white h-2 w-0 group-hover:w-full transition-all duration-300 ml-auto"></div>
+          </span>
       </Link>
     </section>
   );
