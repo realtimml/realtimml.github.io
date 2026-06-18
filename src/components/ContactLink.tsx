c4d5d1f27119397a29d52c166665f2f9f14dbc03
realtimml <timmyphan066@gmail.com>
2026-06-18 01:01:37 -0700

    Make contact page responsive

--- src/components/ContactLink.tsx
+++ src/components/ContactLink.tsx
@@ -9,9 +9,9 @@ export function ContactLink({ icon, label, href, onClick }: ContactLinkProps) {
   const content = (
     <>
       {icon}
-      <span className="text-9xl font-bold">
+      <span className="relative text-5xl md:text-8xl xl:text-9xl font-bold">
         {label}
-        <div className="bg-white h-2 w-0 group-hover:w-full transition-all duration-300 ml-auto"></div>
+        <div className="absolute right-0 bottom-0 bg-white h-2 w-0 group-hover:w-full transition-all duration-300"></div>
       </span>
     </>
   )
@@ -19,7 +19,7 @@ export function ContactLink({ icon, label, href, onClick }: ContactLinkProps) {
   if (onClick) {
     return (
       <button
-        className="flex justify-between group w-full cursor-pointer"
+        className="flex items-center justify-between group w-full cursor-pointer"
         onClick={onClick}
       >
         {content}
@@ -29,7 +29,7 @@ export function ContactLink({ icon, label, href, onClick }: ContactLinkProps) {
 
   return (
     <a
-      className="flex justify-between group"
+      className="flex items-center justify-between group w-full"
       href={href}
       target="_blank"
       rel="noopener noreferrer"
--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -10,19 +10,19 @@ function ContactPage() {
   const encoded = "dGltbXlwaGFuMDY2QGdtYWlsLmNvbQ==";
 
   return (
-    <section className="min-h-screen w-full bg-[#9EA1D4] px-28 pt-20">
+    <section className="min-h-screen w-full bg-[#9EA1D4] px-10 sm:px-28 pt-20">
       <ContactLink
-        icon={<RiGithubFill className="w-40 h-40" />}
+        icon={<RiGithubFill className="w-20 h-20 md:w-32 md:h-32 xl:w-40 xl:h-40" />}
         label="GitHub"
         href="https://github.com/realtimml"
       />
       <ContactLink
-        icon={<RiLinkedinBoxFill className="w-40 h-40" />}
+        icon={<RiLinkedinBoxFill className="w-20 h-20 md:w-32 md:h-32 xl:w-40 xl:h-40" />}
         label="LinkedIn"
         href="https://www.linkedin.com/in/timmy-p/"
       />
       <ContactLink
-        icon={<RiMailFill className="w-40 h-40" />}
+        icon={<RiMailFill className="w-20 h-20 md:w-32 md:h-32 xl:w-40 xl:h-40" />}
         label="Email"
         onClick={() => { window.location.href = `mailto:${atob(encoded)}` }}
       />
