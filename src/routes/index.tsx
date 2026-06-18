add70d4b341aea15f0fea8bf5372c6047ad4c538
realtimml <timmyphan066@gmail.com>
2026-06-17 18:36:39 -0700

    Make home page responsive

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,10 +6,10 @@ export const Route = createFileRoute('/')({
 
 function HomePage() {
   return (
-    <section className="min-h-screen w-full bg-[url(src/assets/imgs/Backgrounds/sky.jpg)] bg-cover bg-position-[100%_20%] brightness-90">
+    <section className="min-h-screen w-full bg-[url(src/assets/imgs/Backgrounds/sky.jpg)] bg-cover bg-position-[40%_20%] brightness-90">
       <div className="absolute top-16 right-17">
-        <h1 className="text-8xl font-bold text-white">Timmy Phan</h1>
-        <h2 className="text-4xl font-bold text-white text-right mt-2">Software Dev. @ Irvine, CA</h2>
+        <h1 className="text-4xl sm:text-8xl font-bold text-white text-right">Timmy Phan</h1>
+        <h2 className="text-2xl sm:text-4xl font-semibold sm:font-bold text-white text-right sm:mt-2">Software Dev. @ Irvine, CA</h2>
       </div>
     </section>
   )
