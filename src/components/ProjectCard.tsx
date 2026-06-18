94c6237b00d6332a359d2b717a8f26f815b7629d
realtimml <timmyphan066@gmail.com>
2026-06-18 03:06:38 -0700

    Add animations to project page

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,7 +10,7 @@ interface ProjectCardProps {
 
 export function ProjectCard({ slug, imageUrl, title, award }: ProjectCardProps) {
   return (
-    <Link to={`/projects/${slug}`} className="relative block hover:scale-95 transition-transform duration-300 group overflow-hidden">
+    <Link to={`/projects/${slug}`} className="relative block hover:scale-95 transition-transform duration-300 group overflow-hidden" viewTransition={['vt-fade-in']}>
       <div className='absolute w-full aspect-video bg-white flex items-center justify-center -translate-x-full group-hover:translate-x-0 transition-transform duration-300'>
         <p className='text-5xl text-center font-bold text-[#bed2d2]'>{title}</p>
       </div>
