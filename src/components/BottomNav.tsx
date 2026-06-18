1b0e4badd7fdfea285aba585f374e347af936228
realtimml <timmyphan066@gmail.com>
2026-06-17 19:23:28 -0700

    Add gradiant to nav bar

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -39,22 +39,28 @@ export function BottomNav() {
   }, [pathname])
 
   return (
-    <nav
-      ref={navRef}
-      className={`fixed inset-x-0 bottom-0 mb-25 sm:mb-20 flex h-[1.1lh] gap-5 overflow-x-auto px-10 sm:px-20 text-nowrap font-semibold text-6xl sm:text-7xl text-white/75 divide-x-4 divide-white/75 scrollbar-none transition-opacity duration-300 [&::-webkit-scrollbar]:hidden ${isProjectModalOpen ? 'opacity-25 pointer-events-none' : ''}`}
-    >
-      {navItems.map((item) => (
-        <Link
-          key={item.to}
-          to={item.to}
-          data-nav-to={item.to}
-          activeProps={{ className: 'text-white' }}
-          className="shrink-0 transition-colors hover:text-white pr-5"
-          onClick={(e) => scrollLinkIntoView(e.currentTarget)}
-        >
-          {item.label}
-        </Link>
-      ))}
-    </nav>
+    <>
+      <div
+          aria-hidden
+          className="pointer-events-none fixed inset-x-0 top-128 bottom-0 bg-gradient-to-t from-black/55 via-black/35 to-transparent"
+        />
+      <nav
+        ref={navRef}
+        className={`fixed inset-x-0 bottom-0 mb-25 sm:mb-20 flex h-[1.1lh] gap-5 overflow-x-auto px-10 sm:px-20 text-nowrap font-semibold text-6xl sm:text-7xl text-white/75 divide-x-4 divide-white/75 scrollbar-none transition-opacity duration-300 [&::-webkit-scrollbar]:hidden ${isProjectModalOpen ? 'opacity-25 pointer-events-none' : ''}`}
+      >
+        {navItems.map((item) => (
+          <Link
+            key={item.to}
+            to={item.to}
+            data-nav-to={item.to}
+            activeProps={{ className: 'text-white' }}
+            className="shrink-0 transition-colors hover:text-white pr-5"
+            onClick={(e) => scrollLinkIntoView(e.currentTarget)}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </nav>
+    </>
   )
 }
