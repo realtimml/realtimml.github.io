053c49a827fefd1691a020ac2d3ad2450261ae6a
realtimml <timmyphan066@gmail.com>
2026-06-19 19:09:48 -0700

    Revert "Add status bar sampler for safari"
    This reverts commit bfa02aad7409fcbeecf5b6c71b35fcc90bdfcc19.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { Outlet, createRootRoute, useRouterState } from '@tanstack/react-router'
 import { BottomNav } from '../components/BottomNav'
-import { SafariStatusBarSampler } from '../components/SafariStatusBarSampler'
 import { useBodyScrollLock } from '../utils/useBodyScrollLock'
 
 export const Route = createRootRoute({
@@ -14,16 +13,13 @@ function RootLayout() {
   useBodyScrollLock(!isProjectsRoute)
 
   return (
-    <>
-      <SafariStatusBarSampler />
-      <div
-        className={`bg-zinc-950 text-zinc-50 ${isProjectsRoute ? 'min-h-screen' : 'h-dvh overflow-hidden overscroll-none'}`}
-      >
-        <main className="w-full">
-          <Outlet />
-        </main>
-        <BottomNav />
-      </div>
-    </>
+    <div
+      className={`bg-zinc-950 text-zinc-50 ${isProjectsRoute ? 'min-h-screen' : 'h-dvh overflow-hidden overscroll-none'}`}
+    >
+      <main className="w-full">
+        <Outlet />
+      </main>
+      <BottomNav />
+    </div>
   )
 }
